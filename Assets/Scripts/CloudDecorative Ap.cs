d8e10f9079b3783a5e267b1b4f0ee173ade83853
AnthonyBoyd1916 <148452153+AnthonyBoyd1916@users.noreply.github.com>
2025-04-10 13:18:52 +0100

    IL

--- Assets/Scripts/CloudDecorative Ap.cs
+++ Assets/Scripts/CloudDecorative Ap.cs
@@ -2,7 +2,7 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CloudApperation : MonoBehaviour
+public class CloudDecorativeAp : MonoBehaviour
 {
     public GameObject cloudline, cloud1BeingSpawned, cloud2BeingSpawned, cloud3BeingSpawned;
     public int cloudCount;
@@ -17,7 +17,7 @@ public class CloudApperation : MonoBehaviour
     }
     public void CloudGenerator()
     {
-        for(int i = 0; i < cloudCount; i++)
+        for (int i = 0; i < cloudCount; i++)
         {
             GenerateRandomSpawnLocation();
         }
@@ -31,35 +31,30 @@ public class CloudApperation : MonoBehaviour
             randomPlaceInBounds = new Vector3(UnityEngine.Random.Range(minBoundingArea.x, maxBoundingArea.x), UnityEngine.Random.Range(minBoundingArea.y, maxBoundingArea.y), UnityEngine.Random.Range(minBoundingArea.z, maxBoundingArea.z));
             CreateOneCloud(randomPlaceInBounds);
         }
-            
+
     }
 
     public void CreateOneCloud(Vector3 newCloudSpawnLocation)
     {
         int SpawnChance = Convert.ToInt32(UnityEngine.Random.Range(1f, 3f));
-        float xRotRandB = UnityEngine.Random.rotation.x;
-        float yRotRandB = UnityEngine.Random.rotation.y;
 
-        int xRotRandA = Convert.ToInt32((float)xRotRandB);
-        int yRotRandA = Convert.ToInt32((float)yRotRandB);
+        Vector3 rotRand = new Vector3(UnityEngine.Random.rotation.x, UnityEngine.Random.rotation.x, 180);
+        //float xRotRandB = UnityEngine.Random.rotation.x;
+        //float yRotRandB = UnityEngine.Random.rotation.y;
+
+        //int xRotRandA = Convert.ToInt32((float)xRotRandB);
+        //int yRotRandA = Convert.ToInt32((float)yRotRandB);
         if (SpawnChance == 1)
         {
-            Instantiate(cloud1BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(xRotRandB, yRotRandB, 180));
+            Instantiate(cloud1BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(rotRand));
         }
         else if (SpawnChance == 2)
         {
-            Instantiate(cloud2BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(xRotRandB, yRotRandB, 180));
+            Instantiate(cloud1BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(rotRand));
         }
         else if (SpawnChance == 3)
         {
-            Instantiate(cloud3BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(xRotRandB, yRotRandB, 180));
+            Instantiate(cloud1BeingSpawned, newCloudSpawnLocation, Quaternion.Euler(rotRand));
         }
-    }
-        
-
-
-    void Update()
-    {
-        
     }
 }
