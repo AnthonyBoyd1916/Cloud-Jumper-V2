3ba0f57e7914a23b74b56776341bb14fe7d836be
AnthonyBoyd1916 <148452153+AnthonyBoyd1916@users.noreply.github.com>
2025-07-16 14:09:00 +0100

    FirstWaveChanges

---
+++ Assets/Scripts/LeapScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class LeapScript : MonoBehaviour
+{
+    public KeyCode leapKey;
+    public int leapRemaining;
+    public float leapForce, timeBetweenLeaps;
+    public bool isLeaping;
+    public Transform playerMoveDirection;
+    private Rigidbody playerRb;
+
+    public void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        //playerMoveDirection=GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(leapKey) && !isLeaping && leapRemaining >= 1)
+        {
+            isLeaping = true;
+            StartCoroutine(LeapForward());
+        }
+        else if ( Input.GetKepUp(leapKey) && !isLeaping && leapRemaining <= 0 ) { return; } 
+        else { return; }
+    }
+
+    IEnumerator LeapForward()
+    {
+        Vector3 leapDirection;
+        leapDirection = playerMoveDirection.up;
+        playerRb.mass = 0.1f;
+        playerRb.AddForce(leapDirection.normalized * leapForce, ForceMode.VelocityChange);
+        leapRemaining--;
+        yield return new WaitForSeconds(timeBetweenLeaps);
+        playerRb.mass = 1f;
+        isLeaping = false;
+    }
+}
